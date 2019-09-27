56807a103906470510916ced174be1d341f14f0c
hannah-saurusrex <hrose.mcmahon@gmail.com>
2019-09-27 16:01:06 -0600

    installed webpack

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,6 @@
+function person(name, favColor) {
+    console.log("hello my name is " + name + " and my favorite color is " + favColor + ".");
+}
+
+person("John Doe", "blue");
+person("Jane Smigh", "green");
