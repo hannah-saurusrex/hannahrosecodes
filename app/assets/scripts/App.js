f81c84c4c7d94d0b8a69646be859efd85034a36d
hannah-saurusrex <hrose.mcmahon@gmail.com>
2019-10-01 17:36:19 -0600

    added transition properties so the header changes color at a trigger point on medium screens, and transitioned logo

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".work"), "60%"); /* write which element we're selecting (by using jquery class selector), and what offset they should use */
 new RevealOnScroll($(".contact-reveal"), "70%");
+var stickyHeader = new StickyHeader();
