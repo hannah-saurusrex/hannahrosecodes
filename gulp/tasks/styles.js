5cdfa28bac30e99f3579cb70005900cccd81d772
hannah-saurusrex <hrose.mcmahon@gmail.com>
2019-10-04 11:05:30 -0600

    added border around portfolio images; adjusted postcss imports

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,9 +1,9 @@
 var gulp = require('gulp'),
+  cssImport = require('postcss-import'),
   postcss = require('gulp-postcss'),
   autoprefixer = require('autoprefixer'),
   cssvars = require('postcss-simple-vars'),
   nested = require('postcss-nested'),
-  cssImport = require('postcss-import'),
   mixins = require('postcss-mixins'),
   hexrgba = require('postcss-hexrgba');
   
